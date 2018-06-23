d9a3b9a3f85e61bc49d21f1d441b26864f24b9d3
bokuweb <bokuweb12@gmail.com>
2018-06-23 17:38:00 +0900

    fix: build config

--- scripts/prod.common.js
+++ scripts/prod.common.js
@@ -1,22 +1,24 @@
-import babel from 'rollup-plugin-babel';
 import replace from 'rollup-plugin-replace';
+import typescript from 'rollup-plugin-typescript2';
 
 export default {
-  input: 'src/index.js',
+  input: 'src/index.tsx',
   plugins: [
-    babel({
-      plugins: ['external-helpers'],
+    typescript({
+      tsconfig: 'tsconfig.json',
+      exclude: ['*.d.ts', 'stories'],
     }),
     replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
   ],
-  sourcemap: true,
-  exports: 'named',
-  name: 'react-rnd',
-  external: ['react', 're-resizable', 'react-draggable', 'react-dom'],
-  globals: {
-    react: 'React',
-    're-resizable': 'Resizable',
-    'react-draggable': 'Draggable',
-    'react-dom': 'ReactDOM',
+  output: {
+    sourcemap: true,
+    exports: 'named',
+    name: 'react-sortable-pane',
+    globals: {
+      react: 'React',
+      're-resizable': 'Resizable',
+      'react-draggable': 'Draggable',
+    },
   },
+  external: ['react', 're-resizable', 'react-draggable'],
 };
