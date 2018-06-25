f9135dd4581823cd699dbb74c3ca060380ff5fd8
bokuweb <bokuweb12@gmail.com>
2018-06-25 09:40:26 +0900

    chore: add cancel story

--- stories/index.tsx
+++ stories/index.tsx
@@ -21,6 +21,8 @@ import SizePercentControlled from "./size/size-percent-controlled";
 
 import Callbacks from "./callback/callbacks";
 
+import Cancel from "./cancel/cancel";
+
 import SandboxBodySizeToMaxWidth from "./sandbox/bodysize-to-maxwidth";
 
 storiesOf("bare", module).add("bare", () => <Bare />);
@@ -43,6 +45,8 @@ storiesOf("size", module)
   .add("percent controlled", () => <SizePercentControlled />);
 
 storiesOf("callbacks", module).add("callback", () => <Callbacks />);
+
+storiesOf("cancel", module).add("cancel", () => <Cancel />);
 
 storiesOf("sandbox", module)
   .add("body size apply to maxwidth", () => <SandboxBodySizeToMaxWidth />);
