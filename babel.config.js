0a65fd98b736c3aebaf53bf509cfdafca6182eb5
bokuweb <bokuweb@users.noreply.github.com>
2019-06-09 14:42:13 +0900

    Upgrade resizable (#529)
    * chore: upgrade resizable
    
    * chore: upgrade storybook
    
    * upgrade containers
    
    * chore: upgrade deps

---
+++ babel.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  presets: ["@babel/preset-typescript", "@babel/preset-react"],
+  plugins: ["@babel/plugin-transform-modules-commonjs", "@babel/plugin-proposal-class-properties"],
+};
