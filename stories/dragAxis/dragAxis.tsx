8db36a9a53c369c4e2ebb573a6851534ff705a12
victorperalta <peralta.r.victor@gmail.com>
2021-05-18 21:15:06 -0700

    Added new axisGrid story

---
+++ stories/dragAxis/dragAxis.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Rnd } from "../../src";
+import { style } from "../styles";
+
+type State = {
+  x: number;
+  y: number;
+  width: number | string;
+  height: number | string;
+};
+
+export default class dragAxis extends React.Component<{}, State> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      width: 100,
+      height: 100,
+      x: 0,
+      y: 0,
+    };
+  }
+
+  render() {
+    return (
+      <Rnd
+        style={style}
+        size={{ width: this.state.width, height: this.state.height }}
+        position={{ x: this.state.x, y: this.state.y }}
+        onDragStop={(e, d) => {
+          this.setState({ ...this.state, ...d });
+        }}
+        onResizeStop={(e, direction, ref, delta, position) => {
+          this.setState({
+            ...this.state,
+            width: ref.style.width,
+            height: ref.style.height,
+            ...position,
+          });
+        }}
+        dragAxis="x"
+      >
+        001
+      </Rnd>
+    );
+  }
+}
